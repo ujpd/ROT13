f5da548cdb7bf2843d8a2d75605faf8a03d99c2a
ujpd <120785896+ujpd@users.noreply.github.com>
2022-12-17 18:42:13 +0545

    rotNo.cpp complete

--- ROT13.cpp
+++ ROT13.cpp
@@ -1,35 +1,24 @@
 #include<iostream>
 using namespace std;
 
-string filterString(string text){ //returns filtered string
+string substiString(string text,int rotNo){
 
-    for (int i = 0; i < text.size(); i++) {
+    for (int i=0;i<text.length();i++){ //iterates the characters in string
+        for(int j=0;j<rotNo;j++){  //iterates rotNo times
 
-        // Finding the character whose
-        // ASCII value fall under this
-        // range
-        if (text[i] < 'A' || text[i] > 'Z' &&
-            text[i] < 'a' || text[i] > 'z')
-        {
-            // erase function to erase
-            // the character
-            text.erase(i, 1);
-            i--;
-        }
-    }
-    return text;
-}
+            if (text[i] < 'A' || text[i] > 'Z' &&  //if anything other than alphabets breaks the loop
+            text[i] < 'a' || text[i] > 'z'){
+                break;
+            }
 
-string finalString(string text,int rotNo){
-
-    for (int i=0;i<text.length();i++){
-        for(int j=0;j<rotNo;j++){
-            text[i]+=1;
-            if(text[i] > 'Z' && text[i] < 'a'){  //if greater than 'Z' and less than 'a' then make it 'A'
+            else{
+                text[i]+=1;
+                if(text[i] > 'Z' && text[i] < 'a'){  //if greater than 'Z' and less than 'a' then make it 'A'
                 text[i]='A';
             }
-            else if(text[i] > 'z'){ //if greater than 'z' make it 'a'
-                text[i]='a';
+                else if(text[i] > 'z'){ //if greater than 'z' make it 'a'
+                    text[i]='a';
+                }
             }
         }
     }
@@ -37,21 +26,27 @@ string finalString(string text,int rotNo){
 }
 
 int main(){
-int i;
-string s;
+
+    string str;
+    int rotNo;
+
+    cout<<"A simple letter substitution cipher program built in C++ that replaces"<<endl;
+    cout<<"a letter with the nth letter after it in the alphabet."<<endl;
+
+    cout<<endl<<"NOTE:";
+    cout<<endl<<"Select and right click to copy the selected text"<<endl;
+    cout<<"RIGHT CLICK to paste"<<endl;
+
+    cout<<endl<<"Enter the text: ";
+    cin>>str;
+
+    cout<<"Enter ROT no: ";
+    cin>>rotNo;
+
+    string rotFinal= substiString(str,rotNo);
 
 
-cout<<"Enter the text: ";
-cin>>s;
+    cout<<endl<<rotFinal<<endl;
 
-cout<<endl<<"Enter ROT no: ";
-cin>>i;
-
-string filteredString_ = filterString(s);
-
-cout<<filteredString_<<endl;
-
-cout<<finalString(filteredString_,i);
-
-return 0;
+    return 0;
 }
